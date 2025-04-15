68b27a77b3f582fcab6fd1006a2d91e0fd47a673
darkops-cpu <darksoul00900@gmail.com>
2025-04-15 06:02:07 +0530

    Update in folder structure, and addition of day 2

---
+++ Month_1/Week_1/Day_1/Practice.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    // if (argc <=1){
+    //     printf("Enter a CLI prompt after the file.\n");
+    // }
+    // else
+    // {
+    //     for (int i = 0; i < argc; i++)
+    //     {
+    //         printf("%s\n", argv[i]);
+    //     }
+    // }
+
+
+    // float c;
+    // c = getchar();
+    // while(c != EOF)
+    // {
+    //     putchar(c);
+    //     c = getchar();
+    // }
+    
+}
