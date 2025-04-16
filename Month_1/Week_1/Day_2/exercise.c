30300158f4cce43b21cc04b02a1463952099ee18
darkops-cpu <darksoul00900@gmail.com>
2025-04-16 07:19:14 +0530

    changes to week1

---
+++ Month_1/Week_1/Day_2/exercise.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main (void)
+{
+    // //ex1
+    // printf("hello world \n");
+
+    // //ex2
+    // printf(" \t\n");
+    
+    // //ex3
+
+    // float fah, cel;
+    // fah = 0;
+    // int upper = 200;
+    // printf("fah \t cel\n");
+
+    // for(; fah <=upper; fah += 20)
+    // {
+    //     cel = 5/9.0*(fah - 32);
+    //     printf("%6.2f %6.2f\n", fah, cel);
+    // }    
+
+    //e4
+
+    // float fah, cel;
+    // cel = 0;
+    // int upper = 200;
+    // printf("fah \t cel\n");
+
+    // for(; cel <=upper; cel += 20)
+    // {
+    //     fah = 9/5.0*(cel + 32);
+    //     printf("%6.2f %6.2f\n", fah, cel);
+    // }
+
+    //ex5
+
+    int c = getchar();
+
+    while (1<2)
+    {
+        if (c!)
+        {
+            /* code */
+        }
+        
+    }
+    
+    
+
+   
+}
